d3537b19c7801f84ec839666fb1563beff6c7154
hiryleu0 <mikoajryll@gmail.com>
2021-02-03 19:42:23 +0100

    interpoled triangle

--- 3DGraphicsOpenGL/main.cpp
+++ 3DGraphicsOpenGL/main.cpp
@@ -12,10 +12,10 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 float vertices[] = {
-	-0.5, -0.5, 0.0,
-	0.5, -0.5, 0.0,
-	0.0, 0.5, 0.0,
-	0.0, -0.7, 0.0
+	-0.5, -0.5, 0.0, 1, 0, 0,
+	0.5, -0.5, 0.0, 0, 1, 0,
+	0.0, 0.5, 0.0, 0, 0, 1,
+	0.0, -0.7, 0.0, 0, 0, 1
 };
 
 unsigned int indices[] = {
@@ -25,24 +25,29 @@ unsigned int indices[] = {
 
 const char* vertex_shader_source = "#version 330 core\n"
 "layout (location =0) in vec3 aPos;\n"
+"layout (location =1) in vec3 aCol;\n"
+"out vec3 newColor;\n"
 "void main()\n"
 "{\n"
 "	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"	newColor = aCol;\n"
 "}\0";
 
 const char* fragment_shader_source1 = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 newColor;\n"
 "uniform vec4 myColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = myColor;\n"
+"   FragColor = vec4(newColor,1.0);\n"
 "}\n\0";
 
 const char* fragment_shader_source2 = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 newColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(4.0f, 0.2f, 0.8f, 1.0f);\n"
+"   FragColor = vec4(newColor,1.0);\n"
 "}\n\0";
 
 
@@ -148,8 +153,10 @@ int main(int argc, char** argv)
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO2);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices)/2, indices+3, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
 	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
 
 	glBindVertexArray(0);
 
@@ -162,17 +169,17 @@ int main(int argc, char** argv)
 
 
 		glUseProgram(shader_program1);
-		float timeValue = glfwGetTime();
-		float greenValue = sin(timeValue) / 2.0f + 0.5f;
-		int vertexColorLocation = glGetUniformLocation(shader_program1, "myColor");
+		//float timeValue = glfwGetTime();
+		//float greenValue = sin(timeValue) / 2.0f + 0.5f;
+		//int vertexColorLocation = glGetUniformLocation(shader_program1, "myColor");
 
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 		glBindVertexArray(VAO);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO1);
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO2);
 		glUseProgram(shader_program2);
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 
